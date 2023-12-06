56f9b8e2be220773a7540c778b6c33bd2dde054e
lucas <lucaswinyb@gmail.com>
2023-12-05 21:05:31 -0300

    alterar menu de acordo com rota

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  public iconEditor = '';
+  public iconCommunity = '';
+  public path = '';
+
+  constructor(private router: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.path = this.router.snapshot.url.map(segment => segment.path).join('/');
+    this.iconActivatedCommunity();
+    this.iconActivatedEditor();
+  }
+
+  iconActivatedEditor()
+  {
+    if(this.path === 'editor-codigo'){
+      this.iconEditor = '../../assets/IconCodeActivated.png'
+      this.iconCommunity = '../../assets/CommunityDesactivated.png'
+    }
+  }
+
+  iconActivatedCommunity()
+  {
+    if(this.path === 'comunidade'){
+      this.iconEditor = '../../assets/IconCodeDesactivated.png'
+      this.iconCommunity = '../../assets/CommunityActivated.png'
+    }
   }
 
 }
